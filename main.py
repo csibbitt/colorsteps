2053648fa85c854645ae14d3b8b65de55659c6f9
Chris Sibbitt <csibbitt@gmail.com>
2021-08-13 00:44:14 -0400

    multi-palette + readout

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
-from palettes import CGA, EGA
+from pygame import Color
+from palettes import ALL_PALETTES
 
 pygame.init()
 screen = pygame.display.set_mode([800, 600])
+font = pygame.font.SysFont(None, 25)
 
+palette_index = 0
 color_index = 0
-screen.fill(EGA[color_index])
+screen.fill(ALL_PALETTES[palette_index]['colors'][color_index])
 pygame.display.flip()
 
 running = True
@@ -14,13 +17,30 @@ while running:
         if i.type == pygame.QUIT or (i.type == pygame.KEYUP and i.key == pygame.K_q):
             running = False
             pygame.quit()
+            break
 
         if i.type == pygame.KEYUP and i.key == pygame.K_UP:
-            color_index = (color_index + 1) % len(EGA)
-            screen.fill(EGA[color_index])
-            pygame.display.flip()
+            color_index = (color_index + 1) % len(ALL_PALETTES[palette_index]['colors'])
 
         if i.type == pygame.KEYUP and i.key == pygame.K_DOWN:
-            color_index = (color_index - 1) % len(EGA)
-            screen.fill(EGA[color_index])
-            pygame.display.flip()
+            color_index = (color_index - 1) % len(ALL_PALETTES[palette_index]['colors'])
+
+        if i.type == pygame.KEYUP and i.key == pygame.K_RIGHT:
+            palette_index = (palette_index + 1) % len(ALL_PALETTES)
+            color_index = 0
+
+        if i.type == pygame.KEYUP and i.key == pygame.K_LEFT:
+            palette_index = (palette_index - 1) % len(ALL_PALETTES)
+            color_index = 0
+
+        cur_color = ALL_PALETTES[palette_index]['colors'][color_index]
+        screen.fill(cur_color)
+        text = font.render(ALL_PALETTES[palette_index]['name'] +
+            '[' + str(color_index) + '](' +
+            hex(cur_color.r) + ',' +
+            hex(cur_color.g) + ',' +
+            hex(cur_color.b) +
+            ')', True, Color('blue'))
+        screen.blit(text, (0, 0))
+        pygame.display.flip()
+        pygame.display.update()
