5a197d2423b10fc3b882008a8bdb1854f7d6e6ad
Chris Sibbitt <csibbitt@gmail.com>
2021-08-13 00:16:18 -0400

    EGA + up/down/quit

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
-from palettes import CGA
+from palettes import CGA, EGA
 
 pygame.init()
-screen = pygame.display.set_mode([800,600])
+screen = pygame.display.set_mode([800, 600])
 
 color_index = 0
-screen.fill(CGA[color_index])
+screen.fill(EGA[color_index])
 pygame.display.flip()
 
-while True:
-  for i in pygame.event.get():
-      if i.type == pygame.QUIT:
-        running = False
-        pygame.quit()
-      if i.type == pygame.KEYUP and i.key == pygame.K_SPACE:
-        color_index = (color_index + 1) % len(CGA)
-        screen.fill(CGA[color_index])
-        pygame.display.flip()
+running = True
+while running:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT or (i.type == pygame.KEYUP and i.key == pygame.K_q):
+            running = False
+            pygame.quit()
+
+        if i.type == pygame.KEYUP and i.key == pygame.K_UP:
+            color_index = (color_index + 1) % len(EGA)
+            screen.fill(EGA[color_index])
+            pygame.display.flip()
+
+        if i.type == pygame.KEYUP and i.key == pygame.K_DOWN:
+            color_index = (color_index - 1) % len(EGA)
+            screen.fill(EGA[color_index])
+            pygame.display.flip()
--- palettes.py
+++ palettes.py
@@ -1,18 +1,91 @@
 from pygame import Color
-CGA = [None] * 16
-CGA[0] = Color('#000000')
-CGA[1] = Color('#0000aa')
-CGA[2] = Color('#00aa00')
-CGA[3] = Color('#00aaaa')
-CGA[4] = Color('#aa0000')
-CGA[5] = Color('#aa00aa')
-CGA[6] = Color('#aa5500')
-CGA[7] = Color('#aaaaaa')
-CGA[8] = Color('#555555')
-CGA[9] = Color('#5555ff')
-CGA[10] = Color('#55ff55')
-CGA[11] = Color('#55ffff')
-CGA[12] = Color('#ff5555')
-CGA[13] = Color('#ff55ff')
-CGA[14] = Color('#ffff55')
-CGA[15] = Color('#ffffff')
+CGA = [
+    Color('#000000'),
+    Color('#0000aa'),
+    Color('#00aa00'),
+    Color('#00aaaa'),
+    Color('#aa0000'),
+    Color('#aa00aa'),
+    Color('#aa5500'),
+    Color('#aaaaaa'),
+    Color('#555555'),
+    Color('#5555ff'),
+    Color('#55ff55'),
+    Color('#55ffff'),
+    Color('#ff5555'),
+    Color('#ff55ff'),
+    Color('#ffff55'),
+    Color('#ffffff')
+]
+
+EGA = [
+    Color('#000000'),
+    Color('#000040'),
+    Color('#000080'),
+    Color('#0000C0'),
+    Color('#004000'),
+    Color('#004040'),
+    Color('#004080'),
+    Color('#0040C0'),
+    Color('#008000'),
+    Color('#008040'),
+    Color('#008080'),
+    Color('#0080C0'),
+    Color('#00C000'),
+    Color('#00C040'),
+    Color('#00C080'),
+    Color('#00C0C0'),
+
+    Color('#400000'),
+    Color('#400040'),
+    Color('#400080'),
+    Color('#4000C0'),
+    Color('#404000'),
+    Color('#404040'),
+    Color('#404080'),
+    Color('#4040C0'),
+    Color('#408000'),
+    Color('#408040'),
+    Color('#408080'),
+    Color('#4080C0'),
+    Color('#40C000'),
+    Color('#40C040'),
+    Color('#40C080'),
+    Color('#40C0C0'),
+
+    Color('#800000'),
+    Color('#800040'),
+    Color('#800080'),
+    Color('#8000C0'),
+    Color('#804000'),
+    Color('#804040'),
+    Color('#804080'),
+    Color('#8040C0'),
+    Color('#808000'),
+    Color('#808040'),
+    Color('#808080'),
+    Color('#8080C0'),
+    Color('#80C000'),
+    Color('#80C040'),
+    Color('#80C080'),
+    Color('#80C0C0'),
+
+    Color('#C00000'),
+    Color('#C00040'),
+    Color('#C00080'),
+    Color('#C000C0'),
+    Color('#C04000'),
+    Color('#C04040'),
+    Color('#C04080'),
+    Color('#C040C0'),
+    Color('#C08000'),
+    Color('#C08040'),
+    Color('#C08080'),
+    Color('#C080C0'),
+    Color('#C0C000'),
+    Color('#C0C040'),
+    Color('#C0C080'),
+    Color('#C0C0C0')
+]
+
+ALL_PALETTES = [{'name': 'CGA', 'colors': CGA}, {'name': 'EGA', 'colors': EGA}]
